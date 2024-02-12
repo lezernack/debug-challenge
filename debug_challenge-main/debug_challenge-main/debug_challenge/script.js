086ecb222b8079e5763f12d10252f968556c0767
lezernack <luks.ezck@gmail.com>
2024-02-12 13:31:04 -0700

    fixed the code

--- debug_challenge-main/debug_challenge-main/debug_challenge/script.js
+++ debug_challenge-main/debug_challenge-main/debug_challenge/script.js
@@ -1,14 +1,17 @@
 // make sure these match the HTML element names for the ID then class.
-let input = document.querySelector("#NAME");
-let display = document.querySelector(".displaymycode");
+let input = document.querySelector("#name");
+let display = document.querySelector(".displayMyName");
 
 // This code should be working
 input.addEventListener("input", function () {
-//   const name = input.value;
-//   display.textContent = name;
-// });
+  const name = input.value;
+  display.textContent = name;
+});
 
 // missing function?
+function simpleMath() {
+  // you should see 5 in the output of your console.
+  console.log(2 + 3);
+}
 
-// you should see 5 in the output of your console
-console.log(simpleMath(2, 3));
+simpleMath();
